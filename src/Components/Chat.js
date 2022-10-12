ec680880b00d11d1cf654c58011beb461123dc7b
RoeiY <roeiyaacobi@gmail.com>
2022-10-12 15:03:07 +0300

    Feature: scroll bottom on new message

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,29 +1,42 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Message from "./Message";
 import ChatButtons from "./ChatButtons";
-import { getActivity, getAllActivities, getInitialMessages } from "../activityUtils";
-import "./Chat.css"
+import {
+  getActivity,
+  getAllActivities,
+  getInitialMessages,
+} from "../activityUtils";
+import "./Chat.css";
 
 let activities = getAllActivities();
 
 const Chat = () => {
-  const [messages, setMessages] = useState(() => getInitialMessages(activities));
+  const [messages, setMessages] = useState(() =>
+    getInitialMessages(activities)
+  );
   const [isDone, setIsDone] = useState(false);
   const [isBotTyping, setIsBotTyping] = useState(false);
+  const chatRef = useRef(null);
 
   const addNewMessage = (autor, text) => {
     setMessages((prevMessages) => {
       return [...prevMessages, { autor: autor, text: text }];
     });
+    setTimeout(() => {
+      chatRef.current.scroll({
+        top: chatRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }, 0);
   };
 
   const noClickHandler = () => {
     addNewMessage("human", "לא");
     setIsBotTyping(true);
-    setTimeout(()=>{
+    setTimeout(() => {
       addNewMessage("bot", getActivity(activities));
       setIsBotTyping(false);
-    },500)
+    }, 500);
   };
 
   const yesClickHandler = () => {
@@ -39,14 +52,19 @@ const Chat = () => {
   };
 
   return (
-    <div className="chat">
+    <div className="chat" ref={chatRef}>
       <ul className="messages-list">
         {messages.map((message, index) => {
           return <Message key={index} message={message} />;
         })}
       </ul>
       {isBotTyping ? <h4>מקליד...</h4> : null}
-      <ChatButtons isDone={isDone} restart={restart} yes={yesClickHandler} no={noClickHandler}/>
+      <ChatButtons
+        isDone={isDone}
+        restart={restart}
+        yes={yesClickHandler}
+        no={noClickHandler}
+      />
     </div>
   );
 };
