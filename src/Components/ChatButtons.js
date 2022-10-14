6e54819d189c58ac025dd0f48b80fdb4c7556ba4
Roei Yaacobi <roeiyaacobi@gmail.com>
2022-10-14 10:54:17 +0300

    Feature: Add confetti

--- src/Components/ChatButtons.js
+++ src/Components/ChatButtons.js
@@ -1,15 +1,23 @@
-import "./ChatButtons.css"
+import startConfetti from "../utils/confetti";
+import "./ChatButtons.css";
 
 const ChatButtons = (props) => {
   const { isDone, restart, yes, no } = props;
   return isDone ? (
     <div className="chat-buttons">
-      <button>מעולה!!</button>
+      <button onClick={(e) => startConfetti(e.target)}>מעולה!!</button>
       <button onClick={() => restart()}>הפעלה מחדש</button>
     </div>
   ) : (
     <div className="chat-buttons">
-      <button onClick={() => yes()}>כן 👍</button>
+      <button
+        onClick={(e) => {
+          yes();
+          startConfetti(e.target);
+        }}
+      >
+        כן 👍
+      </button>
       <button onClick={() => no()}> לא, תציע לי משהו אחר 👎</button>
     </div>
   );
