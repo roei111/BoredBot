55e732975ba290e62e2987c5438a958a80723e61
RoeiY <roeiyaacobi@gmail.com>
2022-10-16 18:21:15 +0300

    Feature: Add links to activities

--- src/utils/activity.js
+++ src/utils/activity.js
@@ -1,9 +1,13 @@
 const defaultActivities = [
-  "תכין לאכול משהו שמעולם לא הכנת",
-  "תפתח בלוג בנושא שאתה נלהב ממנו",
-  "תשיר שיר",
-  "תצא לרכיבה על אופניים",
-  "תשנה את הסיסמה שלך",
+  { text: "תכין לאכול משהו שמעולם לא הכנת", link: "" },
+  { text: "תשנה את הסיסמה שלך", link: "" },
+  { text: "תצא לרכיבה על אופניים", link: "" },
+  { text: "תשיר שיר", link: "" },
+  { text: "תפתח בלוג בנושא שאתה נלהב ממנו", link: "" },
+  {
+    text: "תלמד להכין ברבור מאוריגמי",
+    link: "https://www.instructables.com/How-to-make-a-Paper-Crane-1/?amp_page=true",
+  },
 ];
 
 export const getAllActivities = () => {
@@ -11,18 +15,21 @@ export const getAllActivities = () => {
 };
 
 export const getActivity = (activities) => {
+  if(activities.length === 0) return {text: "", link: ""}; //There is no more activities
   let randomNum = Math.floor(Math.random() * activities.length);
   const activity = activities.splice(randomNum, 1)[0];
   return activity;
 };
 
 export const getInitialMessages = (activities) => {
+  const { text, link } = getActivity(activities);
   return [
     {
       autor: "human",
       text: "משעמם לי תן לי משהו מעניין לעשות",
+      link: "",
       date: new Date(),
     },
-    { autor: "bot", text: getActivity(activities), date: new Date() },
+    { autor: "bot", text, link, date: new Date() },
   ];
 };
