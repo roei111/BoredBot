74776ff3327ccc837430ba4c799ec79762b9d480
RoeiY <roeiyaacobi@gmail.com>
2022-11-08 21:24:01 +0200

    wip

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,17 +1,27 @@
-import "./Welcome.css"
+import "./Welcome.css";
 
-const Welcome = ()=>{
-  return <div className="welcome-wrapper">
-    <div>
-
-    <h1>שלום 👋</h1>
-    <h2>אני מבין שמשעמם לך!</h2>
-    <p>הבוט שלנו יציע לך דברים מגניבים לעשות. מוזמן לנסות!</p>
+const Welcome = () => {
+  return (
+    <div className="welcome-wrapper">
+      <div>
+        <h1>שלום! 👋</h1>
+        <h2>אני מבין שמשעמם לך!</h2>
+        <p>הבוט שלנו יציע לך דברים מגניבים לעשות. מוזמן לנסות!</p>
+      </div>
+      <div className="bottom-text">
+        <p>
+          נוצר על ידי{" "}
+          <a
+            href="https://www.linkedin.com/in/roei-yaacobi/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            רועי יעקובי
+          </a>
+        </p>
+      </div>
     </div>
-    <div>
-      <p>נוצר על ידי <a>רועי יעקובי</a></p>
-    </div>
-  </div>
-}
+  );
+};
 
 export default Welcome;
