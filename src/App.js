1fb9a456464d063c3a455aa783256b6c003ce3d7
Roei <roeiyaacobi@gmail.com>
2022-11-08 20:36:53 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Chat from "./Components/Chat";
 import "./App.css";
+import Welcome from "./Components/Welcome";
 
 function App() {
   return (
     <div className="main">
       <div className="contentWrapper">
+        <Welcome />
         <Chat />
       </div>
     </div>
---
+++ src/Components/Welcome.js
@@ -0,0 +1,17 @@
+import "./Welcome.css"
+
+const Welcome = ()=>{
+  return <div className="welcome-wrapper">
+    <div>
+
+    <h1>שלום 👋</h1>
+    <h2>אני מבין שמשעמם לך!</h2>
+    <p>הבוט שלנו יציע לך דברים מגניבים לעשות. מוזמן לנסות!</p>
+    </div>
+    <div>
+      <p>נוצר על ידי <a>רועי יעקובי</a></p>
+    </div>
+  </div>
+}
+
+export default Welcome;
